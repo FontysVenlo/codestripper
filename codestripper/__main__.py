8c04c57dce3242f570562f9ade9b20dcf0ab52b6
Bonajo <m.bonajo@fontys.nl>
2023-02-10 20:29:46 +0100

    feat: add parser

--- codestripper/__main__.py
+++ codestripper/__main__.py
@@ -1,20 +1,28 @@
 import argparse
+import os
 
-from codestripper.code_stripper import CodeStripper
-from codestripper.utils import FileUtils, set_logger_level
+from codestripper.code_stripper import CodeStripper, strip_files
+from codestripper.utils import FileUtils, set_logger_level, get_working_directory
 
 
-def add_commandline_arguments(parser: argparse.ArgumentParser) -> any:
+def add_commandline_arguments(parser: argparse.ArgumentParser) -> None:
     """Add command line arguments"""
     # Add positional arguments
     parser.add_argument("include", nargs="+", help="files to include for code stripping (multiple files or glob)")
     # Add optional arguments
-    parser.add_argument("-x", "--exclude", action="append", help="files to include for code stripping (multiple files or glob)", default=[])
-    parser.add_argument("-c", "--comment", action="store", help="comment symbol(s) for the given language", default="//")
+    parser.add_argument("-x", "--exclude", action="append",
+                        help="files to include for code stripping (multiple files or glob)", default=[])
+    parser.add_argument("-c", "--comment", action="store",
+                        help="comment symbol(s) for the given language", default="//")
     parser.add_argument("-v", "--verbosity", action="count", help="increase output verbosity", default=0)
-    parser.add_argument("-o", "--output", action="store", help="the output directory to store the stripped files", default="out")
-    parser.add_argument("-r", "--recursive", action="store_true", help="use recursive globs for include/exclude", default=True)
-    return parser.parse_args()
+    parser.add_argument("-o", "--output", action="store",
+                        help="the output directory to store the stripped files", default="out")
+    parser.add_argument("-r", "--recursive", action="store_true",
+                        help="use recursive globs for include/exclude", default=True)
+    parser.add_argument("-d", "--dry-run", action="store_true",
+                        help="dry run of the codestripper, no output is written", default=False)
+    parser.add_argument("-w", "--working-directory", action="store",
+                        help="set the working directory, relative to pwd", default=os.getcwd())
 
 
 def main() -> None:
@@ -22,19 +30,18 @@ def main() -> None:
 
     # Handle command line arguments
     parser = argparse.ArgumentParser()
-    args = add_commandline_arguments(parser)
+    add_commandline_arguments(parser)
+    args = parser.parse_args()
 
     # Setup the logger
-    logger = "codestripper"
-    set_logger_level(logger, args.verbosity)
+    logger_name = "codestripper"
+    set_logger_level(logger_name, args.verbosity)
 
     # Find the files, based on the command line arguments
-    files = FileUtils(args.include, args.exclude, args.recursive, logger).get_matching_files()
-
+    cwd = get_working_directory(args.working_directory)
+    files = FileUtils(args.include, args.exclude, cwd, args.recursive, logger_name).get_matching_files()
     # Strip all the files
-    for file in files:
-
-        CodeStripper(file, args.comment).strip()
+    strip_files(files, args.comment, args.dry_run)
 
 
 if __name__ == '__main__':
