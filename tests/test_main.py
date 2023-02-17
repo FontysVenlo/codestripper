31034b38a78000d1b35acb4143c8a199fa8fe699
Bonajo <m.bonajo@fontys.nl>
2023-02-17 12:21:25 +0100

    refactor: move cli stuff to own file

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,12 +2,14 @@ import glob
 import logging
 import os
 import shutil
+import sys
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 from _pytest.logging import LogCaptureFixture
 
-from codestripper.__main__ import main
+from codestripper.cli import main
 
 test_project_dir = os.path.join(Path(__file__).parent.absolute())
 
@@ -15,10 +17,11 @@ test_project_dir = os.path.join(Path(__file__).parent.absolute())
 def test_main(monkeypatch: pytest.MonkeyPatch, caplog: LogCaptureFixture):
     monkeypatch.chdir(test_project_dir)
     shutil.rmtree("out")
-    args = ["-c", "//", "-x", "*.class", "-vv", "-o", "out", "-w", "testproject", "**/*.java"]
-    with caplog.at_level(logging.INFO, logger='codestripper'):
-        main(args)
-        info = [rec.message for rec in caplog.records]
+    args = ["filename", "-c", "//", "-x", "*.class", "-vv", "-o", "out", "-w", "testproject", "**/*.java"]
+    with patch.object(sys, 'argv', args):
+        with caplog.at_level(logging.INFO, logger='codestripper'):
+            main()
+            info = [rec.message for rec in caplog.records]
     files = glob.glob("out/**/*.java", recursive=True)
     assert len(info) == 1 and len(files) == 4
 
