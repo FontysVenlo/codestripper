ed0b117f613abc496a9b9eec00a96802d4780aff
Bonajo <m.bonajo@fontys.nl>
2023-06-15 14:55:48 +0200

    fix: old test with new functionality

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,7 +16,7 @@ test_project_dir = os.path.join(Path(__file__).parent.absolute())
 
 def test_main(monkeypatch: pytest.MonkeyPatch, caplog: LogCaptureFixture):
     monkeypatch.chdir(test_project_dir)
-    shutil.rmtree("out")
+    shutil.rmtree("out", ignore_errors=True)
     args = ["filename", "-c", "//", "-x", "*.class", "-vv", "-o", "out", "-w", "testproject", "**/*.java"]
     with patch.object(sys, 'argv', args):
         with caplog.at_level(logging.INFO, logger='codestripper'):
